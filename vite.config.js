d96287230ee3e453a6a7a9c546e72e85229d22d5
oghioca2 <ovidiughioca97@gmail.com>
2024-01-23 17:52:59 +0200

    Configure React App for deployment to Github Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/portfolio-website/",
   css: {
     modules: {
       localsConvention: "camelCase",
